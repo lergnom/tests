50881a93768f4d6194555069f4f2ea9809803975
lergnom <lergnom@mail.ru>
2021-05-27 16:44:19 +0300

    new test - moved user in new city

--- src/Immutabelnost/Immutabelnost.test.ts
+++ src/Immutabelnost/Immutabelnost.test.ts
@@ -1,15 +1,39 @@
-import {makeHairStyle, UserType} from "./Immutabelnost";
+import {makeHairStyle, setNewAddress, UserType, UserWithLaptopType} from "./Immutabelnost";
 
 test('referense type test', () => {
     let user: UserType = {
         name: 'Dimych',
         hair: 32,
-        address: {title: 'Minsk'}
+        address: {
+            city: 'Minsk',
+            house: 12
+        }
     }
 
 
     const cutUser = makeHairStyle(user, 10)
     expect(cutUser.hair).toBe(22)
+    expect(cutUser.address).toBe(user.address)
 
+})
+
+test('change adress test', () => {
+    let user: UserWithLaptopType = {
+        name: 'Dimych',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {title: 'Dell'}
+    }
+
+
+    const otherUser = setNewAddress(user, 'Apatity')
+
+    expect(otherUser).not.toBe(user)
+    expect(otherUser.address).not.toBe(user.address)
+    expect(otherUser.address.city).toBe('Apatity')
+    expect(otherUser.laptop).toBe(user.laptop)
 })
 
--- src/Immutabelnost/Immutabelnost.ts
+++ src/Immutabelnost/Immutabelnost.ts
@@ -1,7 +1,20 @@
+import {log} from "util";
+
 export type UserType = {
     name: string
     hair: number
-    address: { title: string }
+    address: {
+        city: string
+        house: number
+    }
+}
+
+export type LaptopType = {
+    title: string
+}
+
+export type UserWithLaptopType = UserType & {
+    laptop: LaptopType
 }
 
 export function makeHairStyle(u: UserType, power: number) {
@@ -13,3 +26,21 @@ export function makeHairStyle(u: UserType, power: number) {
     // return  copyUser
 }
 
+export function setNewAddress(u: UserWithLaptopType, city: string) {
+    // return {...u, address: {city: city}}
+
+    const copy = {
+        ...u,
+        address: {
+            ...u.address, city: city
+        }
+    }
+
+    // copy.address = {
+    //     ...u.address,
+    //     city: city
+    // }
+
+    return copy
+
+}
