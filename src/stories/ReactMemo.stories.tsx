e5bfc7a6730a9ae7691b098f0f4d7bba82f32b97
lergnom <lergnom@mail.ru>
2021-06-10 21:17:36 +0300

    React memo

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {useState} from "react";
+
+export default {
+    title: 'React.memo demo',
+}
+
+const NewMessagesCounter = (props: { count: number }) => {
+    return <div>
+        {props.count}
+    </div>
+}
+
+const UsersSecret = (props: { users: Array<string> }) => {
+    console.log("USERS ")
+    return <div>
+        {props.users.map((u, i) => <div key={i}>{u}</div>)}
+    </div>
+}
+
+const Users = React.memo(UsersSecret)
+
+export const Example1 = () => {
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(["Anton", "Valera", "Dimych", "Masha"])
+
+    const addUser = () => {
+        const newUsers = [...users, 'Sveta' + new Date().getTime()]
+        setUsers(newUsers)
+    }
+
+    return <>
+        <button onClick={() => {
+            setCounter(counter + 1)
+        }}>+
+        </button>
+        <button onClick={addUser}>add user
+        </button>
+        <NewMessagesCounter count={counter}/>
+        <Users users={users}/>
+    </>
+
+}
