80ed0a5f89f2860b6bee159385bfebd921e61392
lergnom <lergnom@mail.ru>
2021-08-19 14:11:53 +0300

    add put,delete, update and get qyerys

--- src/Promises/lessons11NJ.ts
+++ src/Promises/lessons11NJ.ts
@@ -10,47 +10,72 @@ const lesson11_3 = () => {
     };
     const axiosInstance = axios.create(configJSPHLD)
     const API = {
+        getPost: (postId: number) => {
+            return axiosInstance.get(`posts/${postId}`)
+        },
         getPosts: () => {
             return axiosInstance.get(`posts`).then(res => (res.data))
         },
+        setNewPost: function (body: string) {
+            return axiosInstance.post(`posts`, {body})
+        },
+        updatePost: (postId: number, body: string) => {
+            return axiosInstance.put(`posts/${postId}`, {body})
+        },
+        deletePost: (postId: number) => {
+            return axiosInstance.delete(`posts/${postId}`)
+        }
 
     }
-    //@ts-ignore
-    let allPosts = [];
-    const postsData = API.getPosts().then(posts => {
-        let i = 0, max = posts.length;
-        for (; i < max; i += 1) {
-            allPosts.push(posts[i]);
-        }
-        //@ts-ignore
-        allPosts.map( post => {
-            console.log(post.title)
-        } )
+
+    //Получение постов
+    API.getPosts()
+        .then(posts => {
+            let allPosts = [];
+            let i = 0, max = posts.length;
+            console.log(posts)
+            for (; i < max; i += 1) {
+                allPosts.push(posts[i]);
+            }
+            allPosts.map(post => {
+                console.log(`---- ${post.title} ----`);
+            });
+        });
+
+    let style = ['padding: 1rem;',
+        'background: linear-gradient( green, white);',
+        'text-shadow: 0 2px orangered;',
+        'font: 1.3rem/3 Georgia;',
+        'color: white;'].join('');
+
+    // Добавление поста
+    API.setNewPost(JSON.stringify({title: 'lergnom', body: 'example', userId: 4}))
+        .then(resolve => {
+            console.warn('ADD POST\n axios POST = ', resolve);
+            //@ts-ignore
+
+            console.log('%c%s', style, resolve.status)
+        });
+
+
+    //Обновление поста
+    API.updatePost(4, JSON.stringify({title: 'lergnom', body: 'lergnom', userId: 4}))
+        .then(response => {
+            console.log(response)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
+    //Получить пост по id
+    API.getPost(1).then(res => {
+        console.log('POST ID ID ', res)
     })
 
-
-
+    API.deletePost(1)
+        .then(res => console.log('POST DELETE', res))
 
 }
 
 export default lesson11_3
 
-//
-// import axios from 'axios';
-//
-// const configOMB = {
-//     baseURL: 'http://www.omdbapi.com/',
-// };
-// const key = '?apikey=7c250346';
-// const axiosInstance = axios.create(configOMB);
-//
-// const API = {
-//     searchFilmsByTitle: (title: string) => {
-//         return axiosInstance.get(`${key}&t=${title}`)
-//             .then(response => response.data)
-//     },
-//     searchFilmsByType: (title: string, type: string) => {
-//         return axiosInstance.get(`${key}&t=${title}&type=${type}`)
-//             .then(response => response.data)
-//     }
-// };
